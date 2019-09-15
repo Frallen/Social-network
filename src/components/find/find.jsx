7bb1c9ad96a563255801bb2c993f4caeaad89bdd
Frallen <bladone439@gmail.com>
2019-09-15 19:55:49 +0300

    add class

--- src/components/find/find.jsx
+++ src/components/find/find.jsx
@@ -2,96 +2,72 @@ import React from "react";
 import classes from "./find.module.scss";
 import * as axios from "axios"
 
+class find extends React.Component{
 
-
-let Find = (props) => {
-
-let loadusers=()=>{ 
-if (props.Search.length===0){
-  axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
-    props.SetUsers(response.data.items)
-})
+  constructor (props){
+    super(props)
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
+      this.props.SetUsers(response.data.items)
+  })
 }
-
-}
-/*
-  axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
-    props.SetUsers(response.data.items)
-  })*/
-
-// src={p.photos.small != null ? p.photos.small : p.photo} 
-// если в api small не рано null то подменить на фото из пропсов
-
-  return <div className={classes.ns}>
-      <div className={classes.container}>
-  { 
-    
-    props.Search.map(p =>
-     
-      <div
-        key={p.id}
-       
+  /*
+    loadusers=()=>{ 
+        if (this.props.Search.length===0){
+          axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
+            this.props.SetUsers(response.data.items)
+        })
+        }
         
-      >
-    <div className={classes.profile}>
-      <div className={classes.avatarbox}>
-      <img src={p.photos.small != null ? p.photos.small : p.photo} alt="" />
+        }*/
+
+
+    render(){
+        return <div className={classes.ns}>
+        <div className={classes.container}>
+    { 
+      
+      this.props.Search.map(p =>
+       
+        <div
+          key={p.id}
+         
+          
+        >
+      <div className={classes.profile}>
+        <div className={classes.avatarbox}>
+        <img src={p.photos.small != null ? p.photos.small : p.photo} alt="" />
+        </div>
+        <div className={classes.info}>
+          <h3 className={classes.name}>{p.name}</h3>
+          <ul>
+            <li>{p.Age}</li>
+            <li>{p.Gender}</li>
+            <li>{p.City}</li>
+          </ul>
+        </div>
+        {p.followed ? (
+          <button
+            className={classes.follow}
+            onClick={() => {
+              this.props.follow(p.id);
+            }}>Follow</button>
+        ) : (
+          <button
+            className={classes.follow}
+            onClick={() => {
+              this.props.unfollow(p.id);
+            }}>Unfollow</button>
+        )}
       </div>
-      <div className={classes.info}>
-        <h3 className={classes.name}>{p.name}</h3>
-        <ul>
-          <li>{p.Age}</li>
-          <li>{p.Gender}</li>
-          <li>{p.City}</li>
-        </ul>
       </div>
-      {p.followed ? (
-        <button
-          className={classes.follow}
-          onClick={() => {
-            props.follow(p.id);
-          }}>Follow</button>
-      ) : (
-        <button
-          className={classes.follow}
-          onClick={() => {
-            props.unfollow(p.id);
-          }}>Unfollow</button>
       )}
     </div>
+    <button onClick={this.loadusers} className={classes.loadbutton}>
+    Load more
+  </button>
     </div>
-    )}
-  </div>
-  <button onClick={loadusers} className={classes.loadbutton}>
-  Load more
-</button>
-  </div>
-}
 
-export default Find;
-/*
-const Findz = props => {
+    }
+} 
 
- let people = props.Search.map(p => 
-    <ListPeople
-      key={p.id}
-      Name={p.Name}
-      Age={p.Age}
-      Gender={p.Gender}
-      City={p.City}
-      photo={p.photo}
-    ></ListPeople>
-  );
-  return (
-    <div className={classes.ns}>
-      <div className={classes.container}>
-      <Find></Find>
-      </div>
-      <button onClick="" className={classes.loadbutton}>
-        Load more
-      </button>
-    </div>
-  );
-};
-*/
-
+export default find
